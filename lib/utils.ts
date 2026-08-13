c83002aca373e67c0fcf7ddecda9e6bd38bc328d
Hasanga-Anawarathna <hasangaanawarathna@gmail.com>
2026-08-13 23:24:29 +0530

    feat: add currency formatting utilities for USD and LKR

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,10 +2,30 @@ export function cn(...classes: Array<string | false | null | undefined>) {
   return classes.filter(Boolean).join(" ");
 }
 
-export function formatCurrency(amount: number) {
+export const USD_TO_LKR_RATE = 336;
+
+export function formatUsd(amount: number) {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
     maximumFractionDigits: 0,
   }).format(amount);
+}
+
+export function formatLkr(amount: number) {
+  return new Intl.NumberFormat("en-LK", {
+    style: "currency",
+    currency: "LKR",
+    maximumFractionDigits: 0,
+  })
+    .format(amount)
+    .replace("LKR", "LKR ");
+}
+
+export function formatCurrency(amount: number) {
+  return formatUsd(amount);
+}
+
+export function formatDualCurrency(usdAmount: number) {
+  return `${formatLkr(usdAmount * USD_TO_LKR_RATE)} / ${formatUsd(usdAmount)}`;
 }
