fae0529ce8989dd8a27df660dde544fb0defcf9a
Hasanga Anawarathna <hasangaanawarathna@gmail.com>
2026-08-25 16:43:39 +0530

    Update Next.js configuration for export and base path

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/Explore-Ceylon",
+  assetPrefix: "/Explore-Ceylon/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
